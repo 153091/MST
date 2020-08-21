4230aad65b185ff98971a61b1dc5a95255b37cc0
153091 <xpoint06@gmail.com>
2020-08-21 20:22:21 +0300

    EdgeWeightedGraph begin

--- src/EdgeWeightedGraph.java
+++ src/EdgeWeightedGraph.java
@@ -1,12 +1,49 @@
 import edu.princeton.cs.algs4.In;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class EdgeWeightedGraph {
 
-    // create an empte Graph with V vertices
-    public EdgeWeightedGraph(int V) {}
+    private final int V;
+    private int E;
+    private List<Edge>[] adj;
+
+    // create an empty Graph with V vertices
+    public EdgeWeightedGraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("number of vertices must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (List<Edge>[]) new List[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new LinkedList<>();
+    }
 
     // create Graph from input stream
-    public EdgeWeightedGraph(In in) {}
+    public EdgeWeightedGraph(In in) {
+        if (in == null) throw new IllegalArgumentException("argument is null");
+
+        try {
+            V = in.readInt();
+            adj = (List<Edge>[]) new List[V];
+            for (int v = 0; v < V; v++)
+                adj[v] = new LinkedList<>();
+
+            E = in.readInt();
+            if (E < 0) throw new IllegalArgumentException("number of edges must be nonnegative");
+            for (int i = 0; i < E; i++) {
+                int v = in.readInt();
+                int w = in.readInt();
+                double weight = in.readDouble();
+                Edge e = new Edge(v, w ,weight);
+                addEdge(e);
+            }
+        }
+        catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("invalid input format in constructor", e);
+        }
+    }
 
     // add edge
     public void addEdge(Edge e) {}
