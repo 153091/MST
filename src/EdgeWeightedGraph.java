4b3a74bd7079d9e920bc53157a930fc2ee337954
153091 <xpoint06@gmail.com>
2020-08-21 18:58:11 +0300

    EdgeWeightedGraph begin

---
+++ src/EdgeWeightedGraph.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.In;
+
+public class EdgeWeightedGraph {
+
+    // create an empte Graph with V vertices
+    public EdgeWeightedGraph(int V) {}
+
+    // create Graph from input stream
+    public EdgeWeightedGraph(In in) {}
+
+    // add edge
+    public void addEdge(Edge e) {}
+
+    // edges incident to v
+    public Iterable<Edge> adj(int v) {}
+
+    // all edges in the graph
+    public Iterable<Edge> edges() {}
+
+    // number of vertices
+    public int V() {}
+
+    // number of edges
+    public int E() {}
+
+    // string representation
+    public String toString() {}
+
+    public static void main(String[] args) {
+
+    }
+}
