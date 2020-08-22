c21d972a2181ada6c38451177d0fb5339a58a38d
153091 <xpoint06@gmail.com>
2020-08-22 12:32:36 +0300

    EdgeWeightedGraph Done

--- src/EdgeWeightedGraph.java
+++ src/EdgeWeightedGraph.java
@@ -1,10 +1,13 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 public class EdgeWeightedGraph {
+
+    private static final String NEWLINE = System.getProperty("line.separator");
 
     private final int V;
     private int E;
@@ -30,7 +33,7 @@ public class EdgeWeightedGraph {
             for (int v = 0; v < V; v++)
                 adj[v] = new LinkedList<>();
 
-            E = in.readInt();
+            int E = in.readInt();
             if (E < 0) throw new IllegalArgumentException("number of edges must be nonnegative");
             for (int i = 0; i < E; i++) {
                 int v = in.readInt();
@@ -46,24 +49,69 @@ public class EdgeWeightedGraph {
     }
 
     // add edge
-    public void addEdge(Edge e) {}
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        E++;
+    }
 
     // edges incident to v
-    public Iterable<Edge> adj(int v) {}
+    public Iterable<Edge> adj(int v) {
+        return adj[v];
+    }
+
+    // degree of vertex v
+    public int degree(int v) {
+        return adj[v].size();
+    }
 
     // all edges in the graph
-    public Iterable<Edge> edges() {}
+    public Iterable<Edge> edges() {
+        List<Edge> list = new LinkedList<>();
+        for (int v = 0; v < V; v++) {
+            int selfloops = 0;
+            for (Edge e: adj[v]) {
+                if (e.other(v) > v)
+                    list.add(e);
+                // add only one copy of each self loop (self loops will be consecutive)
+                // добавляем каждый второй селфлуп, чтобы только v-v*, but not v*-v
+                else if (e.other(v) == v) {
+                    if (selfloops % 2 == 0) list.add(e);
+                    selfloops++;
+                }
+            }
+        }
+        return list;
+    }
 
     // number of vertices
-    public int V() {}
+    public int V() {
+        return V;
+    }
 
     // number of edges
-    public int E() {}
+    public int E() {
+        return E;
+    }
 
     // string representation
-    public String toString() {}
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (Edge e: adj[v])
+                s.append(e + " ");
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
 
     public static void main(String[] args) {
-
+        In in = new In(args[0]);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        StdOut.println(G);
     }
 }
